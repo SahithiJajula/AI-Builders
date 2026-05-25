5287bfe08367e39df8499e184c1f071ef5973053
nightwolf <muthuchetty.purvi@gmail.com>
2026-05-25 23:23:53 +0530

    Add ScoreCard component to display title and score

---
+++ ScoreCard.jsx
@@ -0,0 +1,30 @@
+function ScoreCard({
+
+title,
+score
+
+}){
+
+return(
+
+<div className="card">
+
+<h3>
+
+{title}
+
+</h3>
+
+<h1>
+
+{score}
+
+</h1>
+
+</div>
+
+)
+
+}
+
+export default ScoreCard
